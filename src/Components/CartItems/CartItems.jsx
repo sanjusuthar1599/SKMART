23dc2c552babe6d0bf12404272444d42c396d0e6
Sanjay <Sanjusuthar1599@gmail.com>
2025-03-31 18:37:22 +0530

    Update CartItems.jsx

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -2,19 +2,22 @@ import React, { useContext } from "react";
 import { ShopContext } from "../../Context/ShopContext";
 import remove_icon from "../Assets/cart_cross_icon.png";
 import { ToastContainer, toast } from "react-toastify";
+import { Link } from 'react-router-dom';
 
 const CartItems = () => {
-    const { getTotalCartAmount, all_product, cartItem, removeFromCart } = useContext(ShopContext);
+    const { getTotalCartAmount, all_product, cartItem, removeFromCart, increaseItemQuantity, decreaseItemQuantity } = useContext(ShopContext);
+
+    const isCartEmpty = Object.keys(cartItem).length === 0 || all_product.length === 0 || !Object.values(cartItem).some(qty => qty > 0);
 
     return (
         <div className="px-4 md:px-20 py-10 mt-20">
             <h1 className="text-xl font-semibold mb-6">Shopping Cart</h1>
 
             {/* Table */}
-            <div className="overflow-x-auto">
+            <div className="overflow-x-auto h-96 overflow-scroll border rounded-lg">
                 <table className="w-full border-collapse border text-sm">
                     {/* Table Header */}
-                    <thead className="bg-gray-100 text-gray-700">
+                    <thead className="bg-gray-100 text-gray-700 sticky top-0">
                         <tr className="border-b">
                             <th className="py-2 px-4 text-left">Product</th>
                             <th className="py-2 px-4 text-left">Title</th>
@@ -24,38 +27,67 @@ const CartItems = () => {
                             <th className="py-2 px-4 text-left">Remove</th>
                         </tr>
                     </thead>
-
-                    {/* Table Body */}
                     <tbody>
-                        {all_product.map((item) => {
-                            if (cartItem[item.id] > 0) {
-                                return (
-                                    <tr key={item.id} className="border-b text-gray-700">
-                                        <td className="py-3 px-4">
-                                            <img src={item.image} alt="" className="w-12 h-12 rounded-md object-cover" />
-                                        </td>
-                                        <td className="py-3 px-4">{item.name}</td>
-                                        <td className="py-3 px-4">${item.new_price}</td>
-                                        <td className="py-3 px-4">
-                                            <button className="border px-3 py-1">{cartItem[item.id]}</button>
-                                        </td>
-                                        <td className="py-3 px-4">${cartItem[item.id] * item.new_price}</td>
-                                        <td className="py-3 px-4">
-                                            <img
-                                                src={remove_icon}
-                                                onClick={() => {
-                                                    removeFromCart(item.id);
-                                                    toast.error("Item removed from carts!", { position: "top-right", autoClose: 1500 });
-                                                }}
-                                                alt=""
-                                                className="w-5 h-5 cursor-pointer hover:opacity-75"
-                                            />
-                                        </td>
-                                    </tr>
-                                );
-                            }
-                            return null;
-                        })}
+                        {isCartEmpty ? (
+                            <tr>
+                                <td colSpan="6" className="justify-center text-lg text-gray-500 p-4">
+                                    <div className="bg-white rounded-lg flex flex-col items-center justify-center space-y-4">
+                                        <img className="mx-auto h-40" src="https://rukminim2.flixcart.com/www/800/800/promos/16/05/2019/d438a32e-765a-4d8b-b4a6-520b560971e8.png?q=90" alt="Empty cart illustration" />
+                                        <div className="text-center">
+                                            <p className="text-3xl font-semibold text-gray-800">Your cart is empty!</p>
+                                            <p className="text-lg text-gray-600">Add items to it now.</p>
+                                        </div>
+                                        <Link to="/mens" className="bg-[#71cd14] text-white px-8 py-2 rounded-lg font-semibold hover:bg-[#4564ddeb] transition-all duration-300 transform hover:scale-105">
+                                            Shop now
+                                        </Link>
+                                    </div>
+                                </td>
+                            </tr>
+                        ) : (
+                            all_product.map((item) => {
+                                if (cartItem[item.id] > 0) {
+                                    return (
+                                        <tr key={item.id} className="border-b text-gray-700">
+                                            <td className="py-3 px-4">
+                                                <img src={item.image} alt={item.name} className="w-12 h-12 rounded-md object-cover" />
+                                            </td>
+                                            <td className="py-3 px-4">{item.name}</td>
+                                            <td className="py-3 px-4">${item.new_price}</td>
+                                            {/* Quantity Control */}
+                                            <td className="py-3 px-4 flex md:mt-0 mt-14 space-x-2">
+                                                <button 
+                                                    className="bg-gray-200 text-gray-800 rounded-full p-2 hover:bg-gray-300 transition-all duration-300"
+                                                    onClick={() => decreaseItemQuantity(item.id)}
+                                                >
+                                                    <span className="font-bold text-xl">-</span>
+                                                </button>
+                                                <span className="px-4 mt-3">{cartItem[item.id]}</span>
+                                                <button 
+                                                    className="bg-gray-200 text-gray-800 rounded-full p-2 hover:bg-gray-300 transition-all duration-300"
+                                                    onClick={() => increaseItemQuantity(item.id)}
+                                                >
+                                                    <span className="font-bold text-xl">+</span>
+                                                </button>
+                                            </td>
+
+                                            <td className="py-3 px-4">$ {cartItem[item.id] * item.new_price}</td>
+                                            <td className="py-3 px-4">
+                                                <img
+                                                    src={remove_icon}
+                                                    onClick={() => {
+                                                        removeFromCart(item.id);
+                                                        toast.error("Item removed from cart!", { position: "top-right", autoClose: 1500 });
+                                                    }}
+                                                    alt="Remove item"
+                                                    className="w-5 h-5 cursor-pointer hover:opacity-75"
+                                                />
+                                            </td>
+                                        </tr>
+                                    );
+                                }
+                                return null;
+                            })
+                        )}
                     </tbody>
                 </table>
             </div>
@@ -98,7 +130,6 @@ const CartItems = () => {
             </div>
 
             <ToastContainer />
-
         </div>
     );
 };
